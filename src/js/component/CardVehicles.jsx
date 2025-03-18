ff6f976924b6f280ed791793d554bd65d133596b
Juan Manuel Paredes <139470808+juanparedes26@users.noreply.github.com>
2025-03-18 22:05:07 +0000

    imagenes de vehiculos añadidos

--- src/js/component/CardVehicles.jsx
+++ src/js/component/CardVehicles.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import vehicleData from "../../json/vehiculos.json"
 
-// import planetData from "../../json/planets.json"
 
 const CardVehicles= ({name,model,manufacturer,id}) => {
-    // const peopleImage = planetData.planets.find(p => p.id === id)?.image || "https://via.placeholder.com/300";
+const vehicleImage = vehicleData.vehiculos.find(p => p.id === id)?.image || "https://via.placeholder.com/300";
     return (
         <div className="card" style={{width: "18rem",flex:"none",margin:"10px"}}>
-            <img src="..." className="card-img-top" alt={name} style={{height:"25rem"}}/>
+            <img src={vehicleImage}className="card-img-top" alt={name} style={{height:"25rem"}}/>
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text"> Model : {model}</p>
